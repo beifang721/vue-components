a50324c3dd131d2a20879ea5f959f0bfb249187d
zengminghong <1041288269@qq.com>
2021-01-18 22:17:17 +0800

    再次解决打包问题

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -4,18 +4,23 @@ import VIcon from '../../components/v-icon/icon.vue';
 import VTabs from '../../components/v-tabs/tabs.vue';
 import VTabPane from '../../components/v-tabs/tab-pane.vue';
 
-import VScroll from '../../components/v-scroll/main';
-
 export default ({
   Vue,
 }) => {
   Vue.component('v-icon', VIcon);
   Vue.component('v-tabs', VTabs);
   Vue.component('v-tab-pane', VTabPane);
-  Vue.component('v-scroll', VScroll);
 
   // 解决打包时不能使用浏览器内置对象问题
-  import('../../components/v-calendar/calendar.vue').then(function (m) {
-    Vue.component('v-calendar', m.default);
+  Vue.mixin({
+    mounted() {
+      import('../../components/v-scroll/main').then((m) => {
+        Vue.component('v-scroll', m.default);
+      });
+      import('../../components/v-calendar/calendar.vue').then((m) => {
+        Vue.component('v-calendar', m.default);
+      });
+    },
   })
+
 }
